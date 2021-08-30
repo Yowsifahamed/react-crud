9d9d2faca2254d2449b22cca18961d5c09274460
YowsifAhamed <yowsifahamed@gmail.com>
2021-08-30 23:38:10 +0530

    form sync with state

--- src/components/dialog/records-dialog.js
+++ src/components/dialog/records-dialog.js
@@ -5,6 +5,32 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 class Dialog extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "1",
+            address: "2"
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        if (event.target.name == "name") {
+            this.setState({ name: event.target.value });
+        } else if (event.target.name == "address") {
+            this.setState({ address: event.target.value });
+        }
+
+        console.log(this.state, "this.state");
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state);
+        event.preventDefault();
+    }
+
     closeCustomModal = e => {
         this.props.onCloseModal(e);
     };
@@ -15,14 +41,18 @@ class Dialog extends Component {
                 <div className="dialog-content container">
                     <h3 className="update-heading">Update </h3>
                     <span className="cross-icon" onClick={this.closeCustomModal}> <FontAwesomeIcon icon={faTimes} /> </span>
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <div className="form-group">
-                            <label for="exampleInputEmail1" className="input-lable">Email address</label>
-                            <input type="email" className="form-control input-control" id="exampleInputEmail1" placeholder="Enter email" />
+                            <label for="exampleInputEmail1" className="input-lable">Name</label>
+                            <input type="text" name="name" className="form-control input-control" placeholder="Enter name"
+                                value={this.state.name}
+                                onChange={this.handleInputChange} />
                         </div>
                         <div className="form-group">
-                            <label for="exampleInputPassword1" className="input-lable">Password</label>
-                            <input type="password" className="form-control input-control" id="exampleInputPassword1" placeholder="Password" />
+                            <label for="exampleInputPassword1" className="input-lable">Address</label>
+                            <input type="text" name="address" className="form-control input-control" placeholder="Enter address"
+                                value={this.state.address}
+                                onChange={this.handleInputChange} />
                         </div>
                         <button type="submit" className="btn btn-primary submit-btn">Submit</button>
                     </form>
